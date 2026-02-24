b21c9db92efdd7d5d6eee35b0b00e48455a68144
MatCam933282 <davideze25@hotmail.com>
2026-02-24 00:34:44 -0600

    Actualizando link de WhatsApp y diseño en activas

--- app/activas.tsx
+++ app/activas.tsx
@@ -89,7 +89,7 @@ export default function OrdenesActivasScreen() {
     const equipo = ordenSeleccionada.equipos;
     const idOrden = ordenSeleccionada.id;
 
-  const urlAprobacion = `https://tu-taller-web.vercel.app/${idOrden}`; // <--- LINK DINÁMICO
+  const urlAprobacion = `https://mi-taller-app-beta.vercel.app/${idOrden}`; // <--- LINK DINÁMICO
 
   //const mensaje = `Hola, el diagnóstico está listo. Revísalo y autoriza aquí:\n${urlAprobacion}`;
 
